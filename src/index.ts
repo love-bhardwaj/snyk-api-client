0ead3ca7a0461a5024fb3f52e6edd20ea4596d09
Love Bhardwaj <lovebhardwaj@Loves-MacBook-Pro.local>
2021-02-03 01:58:08 -0500

    ADDED: all endpoint for group API next is testing them

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import ClientConfig from './lib/config/clientSettings';
 import General from './lib/api/general/index';
 import User from './lib/api/users/index';
+import Group from './lib/api/groups/index';
 import Project from './lib/api/projects/index';
 
-export { ClientConfig, General, User, Project };
+export { ClientConfig, General, User, Project, Group };
---
+++ src/lib/api/groups/index.ts
@@ -0,0 +1,15 @@
+import viewGroupSettings from './viewGroupSettings';
+import updateGroupSettings from './updateGroupSettings';
+import listMembersInGroup from './listMembersInGroup';
+import addMemberToOrg from './addMemberToOrg';
+import listAllTagsInGroup from './listTagsInGroup';
+import deleteTagFromGroup from './deleteTagFromGroup';
+
+export default {
+  viewGroupSettings,
+  updateGroupSettings,
+  listMembersInGroup,
+  addMemberToOrg,
+  listAllTagsInGroup,
+  deleteTagFromGroup,
+};
