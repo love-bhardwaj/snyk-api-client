889482837719113ca725b81796f9ba5a69ad354d
Love Bhardwaj <lovebhardwaj@Loves-MacBook-Pro.local>
2021-02-05 01:53:28 -0500

    FIX: minor bug fix

--- src/lib/api/integrations/udpateIntegrationSettings.ts
+++ src/lib/api/integrations/udpateIntegrationSettings.ts
@@ -1,7 +1,7 @@
 import getUrl from '../../utils/getUrl';
 import processRequest from '../../utils/processRequest';
 import { RequestMethod, RequestOpts, ReturnData } from '../../../types/types';
-import { RequestBodyEmpty } from 'errors/errors';
+import { RequestBodyEmpty } from '../../../errors/errors';
 
 export default async (data: { orgId: string; integrationId: string }, opts: RequestOpts = {}): Promise<ReturnData> => {
   if (!opts.requestBody) throw new RequestBodyEmpty();
