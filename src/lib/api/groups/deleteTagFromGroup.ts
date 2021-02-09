30341cd01d5f6c9f4924718f5c7a3ff75806549f
Love Bhardwaj <lovebhardwaj@Loves-MacBook-Pro.local>
2021-02-09 00:39:43 -0500

    UPDATE: request opts where body is required updated

--- src/lib/api/groups/deleteTagFromGroup.ts
+++ src/lib/api/groups/deleteTagFromGroup.ts
@@ -1,11 +1,11 @@
 import getUrl from '../../utils/getUrl';
 import isObjectEmpty from '../../utils/isObjectEmpty';
 import processRequest from '../../utils/processRequest';
-import { ReqOpts, RequestMethod, ReturnData } from '../../../types/types';
+import { ReqOptsWithBody, RequestMethod, ReturnData } from '../../../types/types';
 import { RequestBodyEmpty } from '../../../errors/errors';
 
-export default async (data: { groupId: string }, opts: ReqOpts = {}): Promise<ReturnData> => {
-  if (!opts.requestBody || isObjectEmpty(opts.requestBody)) throw new RequestBodyEmpty();
+export default async (data: { groupId: string }, opts: ReqOptsWithBody): Promise<ReturnData> => {
+  if (isObjectEmpty(opts.requestBody)) throw new RequestBodyEmpty();
 
   const { groupId } = data;
   const endpoint = getUrl.deleteTagFromGroup(groupId);
